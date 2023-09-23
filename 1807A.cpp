ce184fddf9152a6fc8c3b069ee5e995ea3966fc9
ifte-kharul <iftekharul.tamim@gmail.com>
2023-09-24 00:51:46 +0600

    1807A from Ubuntu

---
+++ 1807A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,a,b,c;
+    cin>>n;
+    while(n--){
+        cin>>a>>b>>c;
+        if(a+b==c){
+            cout<<"+"<<endl;
+        }else{
+            cout<<"-"<<endl;
+        }
+    }
+    return 0;
+}
